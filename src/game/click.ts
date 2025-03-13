9cbb288e3353296e3ac102de4043a56554bce823
李大凡 <lidafan.me@bytedance.com>
2025-03-13 14:42:42 +0800

    refactor: use effect gen

--- src/game/click.ts
+++ src/game/click.ts
@@ -3,24 +3,22 @@ import { CanvasContext } from './context/canvas';
 import { ChipConfigContext, ChipMatrixContext, Matrix, Vec2 } from './widgets/chip';
 import { updateRef } from './utils/updateRef';
 
-const getPointerTarget = Effect.serviceFunction(
-  Effect.all([CanvasContext, ChipConfigContext]),
-  ([canvas, chipConfig]) =>
-    (event: MouseEvent): Option.Option<Vec2> => {
-      const { x, y } = event;
-      const rect = canvas.context.canvas.getBoundingClientRect();
-      const offsetX = x - rect.x;
-      const offsetY = y - rect.y;
-      const isXHit = offsetX % (chipConfig.gap + chipConfig.size) < chipConfig.size;
-      const isYHit = offsetY % (chipConfig.gap + chipConfig.size) < chipConfig.size;
-      if (isXHit && isYHit) {
-        const targetColumnIndex = Math.ceil(offsetX / (chipConfig.gap + chipConfig.size)) - 1;
-        const targetRowIndex = Math.ceil(offsetY / (chipConfig.gap + chipConfig.size)) - 1;
-        return Option.some(Vec2({ x: targetColumnIndex, y: targetRowIndex }));
-      }
-      return Option.none();
-    },
-);
+const getPointerTarget = (event: MouseEvent) =>
+  Effect.gen(function* () {
+    const [chipConfig, canvas] = yield* Effect.all([ChipConfigContext, CanvasContext]);
+    const { x, y } = event;
+    const rect = canvas.context.canvas.getBoundingClientRect();
+    const offsetX = x - rect.x;
+    const offsetY = y - rect.y;
+    const isXHit = offsetX % (chipConfig.gap + chipConfig.size) < chipConfig.size;
+    const isYHit = offsetY % (chipConfig.gap + chipConfig.size) < chipConfig.size;
+    if (isXHit && isYHit) {
+      const targetColumnIndex = Math.ceil(offsetX / (chipConfig.gap + chipConfig.size)) - 1;
+      const targetRowIndex = Math.ceil(offsetY / (chipConfig.gap + chipConfig.size)) - 1;
+      return Option.some(Vec2({ x: targetColumnIndex, y: targetRowIndex }));
+    }
+    return Option.none();
+  });
 
 const checkIsSibling = (a: Vec2, b: Vec2): boolean => {
   const xDistance = Math.abs(a.x - b.x);
@@ -28,45 +26,40 @@ const checkIsSibling = (a: Vec2, b: Vec2): boolean => {
   return (xDistance === 1 || yDistance === 1) && xDistance + yDistance === 1;
 };
 
-export const handleClickEvent = Effect.serviceFunctionEffect(
-  ChipMatrixContext,
-  chipMatrixState => (event: MouseEvent) =>
-    Effect.gen(function* () {
-      const target = yield* getPointerTarget(event);
-      const current = yield* Ref.get(chipMatrixState.selected);
+export const handleClickEvent = (event: MouseEvent) =>
+  Effect.gen(function* () {
+    const chipMatrix = yield* ChipMatrixContext;
+    const target = yield* getPointerTarget(event);
+    const current = yield* Ref.get(chipMatrix.selected);
 
-      if (Option.isNone(target)) {
-        return;
-      }
+    if (Option.isNone(target)) {
+      return;
+    }
 
-      if (Option.isNone(current)) {
-        yield* Ref.set(chipMatrixState.selected, target);
-        return;
-      }
+    if (Option.isNone(current)) {
+      yield* Ref.set(chipMatrix.selected, target);
+      return;
+    }
 
-      const isSame = Option.all([target, current]).pipe(
-        Option.andThen(([targetPos, currentPos]) => Equal.equals(targetPos, currentPos)),
-        Option.getOrElse(() => false),
-      );
-      if (isSame) {
-        yield* Ref.set(chipMatrixState.selected, Option.none());
-        return;
-      }
+    const [targetPos, currentPos] = Option.all([target, current]).pipe(Option.getOrThrow);
+    const isSame = Equal.equals(targetPos, currentPos);
+    if (isSame) {
+      yield* Ref.set(chipMatrix.selected, Option.none());
+      return;
+    }
 
-      const [targetPos, currentPos] = Option.all([target, current]).pipe(Option.getOrThrow);
-      const isSibling = checkIsSibling(targetPos, currentPos);
-      if (isSibling) {
-        const matrix = yield* Ref.get(chipMatrixState.matrix);
-        const currentChip = Matrix.getItem(matrix, currentPos).pipe(Option.getOrThrow);
-        const targetChip = Matrix.getItem(matrix, targetPos).pipe(Option.getOrThrow);
-        yield* updateRef(chipMatrixState.matrix, draft => {
-          Matrix.setItem(draft, targetPos, currentChip);
-          Matrix.setItem(draft, currentPos, targetChip);
-        });
-        yield* Ref.set(chipMatrixState.selected, Option.none());
-        return;
-      }
+    const isSibling = checkIsSibling(targetPos, currentPos);
+    if (isSibling) {
+      const matrix = yield* Ref.get(chipMatrix.matrix);
+      const currentChip = Matrix.getItem(matrix, currentPos).pipe(Option.getOrThrow);
+      const targetChip = Matrix.getItem(matrix, targetPos).pipe(Option.getOrThrow);
+      yield* updateRef(chipMatrix.matrix, draft => {
+        Matrix.setItem(draft, targetPos, currentChip);
+        Matrix.setItem(draft, currentPos, targetChip);
+      });
+      yield* Ref.set(chipMatrix.selected, Option.none());
+      return;
+    }
 
-      yield* Ref.set(chipMatrixState.selected, Option.some(targetPos));
-    }),
-);
+    yield* Ref.set(chipMatrix.selected, Option.some(targetPos));
+  });
